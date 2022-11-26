647ce30dc72cf14a4c71a5b42c397a3bcc0623fd
Edwing Batz <ebatzm@miumg.edu.gt>
2022-11-26 09:24:41 -0600

    Batz: cree los servlets para el login, LoginController, Logout, NewServlet y la clase en utilidades de la conexion, la clase de Usuario

---
+++ src/java/Usuario.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Utilidades.ConexionBaseDeDatos;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Edwing
+ */
+public class Usuario {
+    
+    private ConexionBaseDeDatos conectorBD;
+    private Connection conexion;
+    private PreparedStatement statement = null;
+    private ResultSet result = null;
+    
+    public Usuario(){
+        this.conectorBD=new ConexionBaseDeDatos();
+    }
+    
+    public String validarUsuario(String user, String pass){
+        String sql = "SELECT * FROM usuario WHERE usuario = '" + user + "' AND contrasenia = '"+pass+"'";
+        try{
+            this.conexion = this.conectorBD.conectar();
+            this.statement = conexion.prepareStatement(sql);
+            this.result=this.statement.executeQuery();
+            if(result != null){
+                 String usuarioEncontrado="";
+                while (result.next()){
+                    usuarioEncontrado= result.getString("usuario");
+                }
+                return usuarioEncontrado;
+            }
+            return "Usuario no encontrado";
+        }
+        catch(SQLException e){
+           return e.getMessage();
+        }
+    }
+    
+}
